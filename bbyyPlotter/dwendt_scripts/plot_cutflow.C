0a74369358f9f41af885fd1047e7a670d4c025c9
David Wendt <dwendt@cern.ch>
2020-08-29 02:17:00 +0200

    Added comments

--- bbyyPlotter/dwendt_scripts/plot_cutflow.C
+++ bbyyPlotter/dwendt_scripts/plot_cutflow.C
@@ -1,9 +1,14 @@
+// Read and plot cutflow histograms
+
+// Written by David Wendt
+
 #include "TFile.h"
 #include "TTree.h"
 #include "TBranch.h"
 #include "TBranchElement.h"
 #include "TH1.h" 
 
+// I/O function to print the bins of a histogram
 void print_histogram(TH1F *hist, int nbins) {
 	cout << "\n\tPrinting " << hist->GetName() << ":" << endl;
 	for (int i = 0; i < nbins; i++) {
@@ -11,6 +16,9 @@ void print_histogram(TH1F *hist, int nbins) {
 	}
 }
 
+/* Find all branches in a root file's CollectionTree
+ * with "cutFlow" in the name, return a vector of 
+ * the names of these branches */
 std::vector<std::string> find_cutflow_branches(std::string fname) {
 	TFile *f = new TFile(fname.c_str());
 	TTree *t = (TTree *)f->Get("CollectionTree");	
@@ -25,14 +33,18 @@ std::vector<std::string> find_cutflow_branches(std::string fname) {
 			cutflow_branches.push_back(name);
 		}
 	}
-
-	for (int i = 0; i < cutflow_branches.size(); i++) {
+	
+	// Optional: Print branch names
+/*	for (int i = 0; i < cutflow_branches.size(); i++) {
 	 	cout << i << ": " << cutflow_branches[i] << endl;
 	}
-	
+*/	
 	return cutflow_branches;
 }
 
+/* Given a root file and name for a cutflow branch, 
+ * plot the original histogram from the branch and also
+ * calculate and plot the properly formatted cutflow histogram */
 void plot_branch(std::string fname, std::string cutflow_branch, std::string label,
 	TPad *lpad, TPad *rpad, std::string tag, int lmax, int rmax) {
 	
@@ -43,23 +55,32 @@ void plot_branch(std::string fname, std::string cutflow_branch, std::string labe
 	std::string hname0 = label + ", initial cutflow";
 	TH1F *hcutflow0 = new TH1F(hname0.c_str(), hname0.c_str(), 7, 0, 7);
 	t->SetBranchAddress(cutflow_branch.c_str(), &cutflow_bin);
+
+	// Print name of branch and filename
 	std::cout << "Branch to read: " << cutflow_branch.c_str() 
 		  << " from file " << fname << endl;
 
-	std::cout << "Reading initial cutflow histogram" << endl;
 	int nentries = (int)t->GetEntries();
 	std::cout << "nentries = " << nentries << endl;
 	for (int i = 0; i < nentries; i++) {
+		// Optional: print progress of loop
 //		std::cout << "Reading entry " << i << " of " << nentries << endl;
 		t->GetEntry(i);
 		hcutflow0->Fill(cutflow_bin);
 	} 
-	std::cout << "Finished reading initial cutflow histogram" << endl;
 			
 	std::string hname = label + ", cutflow";
 	TH1F *hcutflow = new TH1F(hname.c_str(), hname.c_str(), 7, 0, 7);
 
-	std::cout << "Calculating final cutflow histogram" << endl;
+	/* In the cutflow histogram saved in the branch (here denoted
+	 * hcutflow0), each bin corresponds to the number of events 
+	 * removed by a given cut. To produce the standard cutflow histogram
+	 * (denoted hcutflow), we calculate
+	 *
+	 * hcutflow[i] = sum_{j=0}^{nbins} (hcutflow0[j]) - sum_{j=0}^{i-1} (hcutflow0[j])
+	 *
+	 */
+
 	for (int i = 0; i < 8; i++) {
 		int bin_content = hcutflow0->GetEntries();
 
@@ -69,18 +90,15 @@ void plot_branch(std::string fname, std::string cutflow_branch, std::string labe
 
 		hcutflow->SetBinContent(i, bin_content);				
 	}
-	std::cout << "Finished calculating final cutflow histogram" << endl;
-	std::cout << "Branch read: " << cutflow_branch.c_str() 
-		  << " from file " << fname << endl;
 
+	// Labels of the six cuts applied
 	std::vector<std::string> bin_labels = {
 		"2 photons",
 		"lepton veto",
 		"2 cand. jets",
 		"l.t. 6 cent. jets",
 		"2 b jets",
-		"l.t. 3 b jets" /*,
-		"pass everything" */
+		"l.t. 3 b jets"
 	};
 
 	for (int i = 0; i < 6; i++) {
@@ -88,21 +106,23 @@ void plot_branch(std::string fname, std::string cutflow_branch, std::string labe
 		hcutflow->GetXaxis()->SetBinLabel(i + 2, bin_labels[i].c_str());
 	}
 
-	cout << "Scaling by " << 1. / hcutflow->GetBinContent(0) << endl;
-
-	print_histogram(hcutflow0, 7);
-	print_histogram(hcutflow, 7);
+	// Optional: print the content of the histograms 
+	// before/after scaling
+	// print_histogram(hcutflow0, 7);
+	// print_histogram(hcutflow, 7);
 
 	hcutflow0->Scale( 1. / hcutflow->GetBinContent(0));
 	hcutflow->Scale( 1. / hcutflow->GetBinContent(0));
 
-	print_histogram(hcutflow0, 7);
-	print_histogram(hcutflow, 7);
+	// print_histogram(hcutflow0, 7);
+	// print_histogram(hcutflow, 7);
 	
 	lpad->cd();
 	lpad->SetLogy();
 	hcutflow0->Draw(tag.c_str());
-//	hcutflow0->SetMaximum(lmax);
+	// Optional: set the y axis maximum 
+	// (originally implemented before scaling histograms)
+//	hcutflow0->SetMaximum(lmax); 
 	lpad->BuildLegend(0.36, 0.65, 0.64, 0.88);
 
 	rpad->cd();
@@ -127,13 +147,22 @@ void plot_cutflow() {
 	std::vector<std::string> c2vs = {
 		"0", "0p5", "1", "1p5", "2", "4"
 	};
+
+	// Different paths to acces different files. 
+	// Uncommented ones are the ones we decided to end up using
 	
+	// One location:
 	// std::string path = "/eos/atlas/atlascerngroupdisk/phys-hdbs/diHiggs/yybb/skimmed_samples_h025_diphotonselection/mc16d/";
-	std::string path = "/eos/atlas/atlascerngroupdisk/phys-higgs/HSG1/MxAOD/h025/mc16d/Nominal/";
-/*	// std::string fname_ggF = path + "mc16d.aMCnlo_Hwpp_hh_yybb.root";
-	std::string fname_ggF = path + "mc16d.aMCnloHwpp_hh_yybb.MxAODDetailedNoSkim.e4419_s3126_r10201_p4097_h025.root";
+	// std::string fname_ggF = path + "mc16d.aMCnlo_Hwpp_hh_yybb.root";
 	// std::string fname_ttH = path + "mc16d.PowhegPy8_ttH125_fixweight.root";
+	
+	// Other location:
+	std::string path = "/eos/atlas/atlascerngroupdisk/phys-higgs/HSG1/MxAOD/h025/mc16d/Nominal/";
+	std::string fname_ggF = path + "mc16d.aMCnloHwpp_hh_yybb.MxAODDetailedNoSkim.e4419_s3126_r10201_p4097_h025.root";
+	// The first ttH sample is a folder, so 
+	// use of TChain must be implemented to acces it
 	// std::string fname_ttH = path + "mc16d.PowhegPy8_ttH125_fixweight.MxAODDetailed.e7488_s3126_r10201_p4097_h025.root";
+	// Alternate ttH sample
 	std::string fname_ttH = path + "mc16d.aMCnloH7_ttH125.MxAODDetailedNoSkim.e7612_s3126_r10201_p4097_h025.root";
 
 	std::vector<std::string> ggF_branches = find_cutflow_branches(fname_ggF);
@@ -142,11 +171,15 @@ void plot_cutflow() {
 	std::string ggF_cutflow_branch = ggF_branches[10];
 	std::string ttH_cutflow_branch = ttH_branches[10];
 
+	// To change the argument to h->Draw(), edit the third to last argument to plot_branch
+	// To change the ymax on the left and right axes, uncomment the corresponding line
+	// above and change the last two arguments
+	// Same instructions apply to the call to plot_branch below in the c2v loop
 	gStyle->SetHistLineColor(4);
 	plot_branch(fname_ggF, ggF_cutflow_branch, "ggF", pad3, pad4, "text90hist", 300000, 300000);
 	gStyle->SetHistLineColor(2);
 	plot_branch(fname_ttH, ttH_cutflow_branch, "ttH", pad3, pad4, "text90histsame", 300000, 300000);
-*/
+
 	int color = 2;
 
 	std::vector<std::string> c2v_fnames;
@@ -172,15 +205,16 @@ void plot_cutflow() {
 		plot_branch(fname_c2v, c2v_cutflow_branch, "c2v = " + c2v, pad1, pad2, tag, 30000, 30000);
 	}
 
-	std::cout << "\n\tBranches read:" << endl;
-//	std::cout << ggF_cutflow_branch << " from " 
-//		<< fname_ggF << endl;
-//	std::cout << ttH_cutflow_branch << " from " 
-//		<< fname_ttH << endl;
+	// Optional: print list of all branches read
+/* 	std::cout << "\n\tBranches read:" << endl;
+	std::cout << ggF_cutflow_branch << " from " 
+		<< fname_ggF << endl;
+	std::cout << ttH_cutflow_branch << " from " 
+		<< fname_ttH << endl;
 	for (int i = 0; i < c2vs.size(); i++) {
 		std::cout << c2v_cutflow_branches[i] << " from " 
 			<< c2v_fnames[i] << endl;
 	}
-
- 	c1->SaveAs("cutflow_7_0_7_alt_rebinned_sig_log.png");		
+*/
+ 	c1->SaveAs("cutflow.png");		
 }
