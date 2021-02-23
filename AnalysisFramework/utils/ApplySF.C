3bc26455385b77e2b10da9d741bbe94ec6f9b523
jpearkes <jannicke.pearkes@cern.ch>
2021-02-24 00:57:08 +0100

    Adding BDT score to ApplySF

--- AnalysisFramework/utils/ApplySF.C
+++ AnalysisFramework/utils/ApplySF.C
@@ -32,14 +32,14 @@ int main(int argc, char** argv)
 	TString varName=argv[6];
 
 
-	TString dir = "../run/plots/";
+	TString dir = "../run/nonres_paper/";
 
 	TFile * purityFile = new TFile("Nonresonant_purity.root", "read");
 	TH1F * ggHist = (TH1F *) purityFile->Get("gg"); 
 	TH1F * jgHist = (TH1F *) purityFile->Get("jg"); 
 	TH1F * jjHist = (TH1F *) purityFile->Get("jj"); 
 	
-	vector<TString> varNames={"m_yy", "m_jj", "m_yyjj_tilde", "N_j", "N_j_central"};
+	vector<TString> varNames={"m_yy", "m_jj", "m_yyjj_tilde", "N_j", "N_j_central","nonRes_XGBoost_btag77_BCal_withTop_lowMass_Score","nonRes_XGBoost_btag77_BCal_withTop_highMass_Score"};
 
 	TFile * targetFile = new TFile(dir+Form("%s_%s.root", targetName.Data(), regionName.Data()), "read");
 	TH1F * targetHist_1 = (TH1F *) targetFile->Get(Form("sumHisto_%s_%s", varNames[0].Data(), regionName.Data()));
@@ -47,6 +47,8 @@ int main(int argc, char** argv)
 	TH1F * targetHist_3 = (TH1F *) targetFile->Get(Form("sumHisto_%s_%s", varNames[2].Data(), regionName.Data()));
 	TH1F * targetHist_4 = (TH1F *) targetFile->Get(Form("sumHisto_%s_%s", varNames[3].Data(), regionName.Data()));
 	TH1F * targetHist_5 = (TH1F *) targetFile->Get(Form("sumHisto_%s_%s", varNames[4].Data(), regionName.Data()));
+	TH1F * targetHist_6 = (TH1F *) targetFile->Get(Form("sumHisto_%s_%s", varNames[5].Data(), regionName.Data()));
+	TH1F * targetHist_7 = (TH1F *) targetFile->Get(Form("sumHisto_%s_%s", varNames[6].Data(), regionName.Data()));
 
 	TFile * dataFile = new TFile(dir+Form("%s_%s.root", dataName.Data(), regionName.Data()), "read");
 	TH1F * dataHist_1 = (TH1F *) dataFile->Get(Form("sumHisto_%s_%s", varNames[0].Data(), regionName.Data()));
@@ -54,6 +56,8 @@ int main(int argc, char** argv)
 	TH1F * dataHist_3 = (TH1F *) dataFile->Get(Form("sumHisto_%s_%s", varNames[2].Data(), regionName.Data()));
 	TH1F * dataHist_4 = (TH1F *) dataFile->Get(Form("sumHisto_%s_%s", varNames[3].Data(), regionName.Data()));
 	TH1F * dataHist_5 = (TH1F *) dataFile->Get(Form("sumHisto_%s_%s", varNames[4].Data(), regionName.Data()));
+	TH1F * dataHist_6 = (TH1F *) dataFile->Get(Form("sumHisto_%s_%s", varNames[5].Data(), regionName.Data()));
+	TH1F * dataHist_7 = (TH1F *) dataFile->Get(Form("sumHisto_%s_%s", varNames[6].Data(), regionName.Data()));
 
 	TFile * ttyyallFile = new TFile(dir+Form("%s_%s.root", ttyyallName.Data(), regionName.Data()), "read");
 	TH1F * ttyyallHist_1 = (TH1F *) ttyyallFile->Get(Form("sumHisto_%s_%s", varNames[0].Data(), regionName.Data()));
@@ -61,6 +65,8 @@ int main(int argc, char** argv)
 	TH1F * ttyyallHist_3 = (TH1F *) ttyyallFile->Get(Form("sumHisto_%s_%s", varNames[2].Data(), regionName.Data()));
 	TH1F * ttyyallHist_4 = (TH1F *) ttyyallFile->Get(Form("sumHisto_%s_%s", varNames[3].Data(), regionName.Data()));
 	TH1F * ttyyallHist_5 = (TH1F *) ttyyallFile->Get(Form("sumHisto_%s_%s", varNames[4].Data(), regionName.Data()));
+	TH1F * ttyyallHist_6 = (TH1F *) ttyyallFile->Get(Form("sumHisto_%s_%s", varNames[5].Data(), regionName.Data()));
+	TH1F * ttyyallHist_7 = (TH1F *) ttyyallFile->Get(Form("sumHisto_%s_%s", varNames[6].Data(), regionName.Data()));
 
 	TFile * ttyynoFile = new TFile(dir+Form("%s_%s.root", ttyynoName.Data(), regionName.Data()), "read");
 	TH1F * ttyynoHist_1 = (TH1F *) ttyynoFile->Get(Form("sumHisto_%s_%s", varNames[0].Data(), regionName.Data()));
@@ -68,7 +74,8 @@ int main(int argc, char** argv)
 	TH1F * ttyynoHist_3 = (TH1F *) ttyynoFile->Get(Form("sumHisto_%s_%s", varNames[2].Data(), regionName.Data()));
 	TH1F * ttyynoHist_4 = (TH1F *) ttyynoFile->Get(Form("sumHisto_%s_%s", varNames[3].Data(), regionName.Data()));
 	TH1F * ttyynoHist_5 = (TH1F *) ttyynoFile->Get(Form("sumHisto_%s_%s", varNames[4].Data(), regionName.Data()));
-
+	TH1F * ttyynoHist_6 = (TH1F *) ttyynoFile->Get(Form("sumHisto_%s_%s", varNames[5].Data(), regionName.Data()));
+	TH1F * ttyynoHist_7 = (TH1F *) ttyynoFile->Get(Form("sumHisto_%s_%s", varNames[6].Data(), regionName.Data()));
 
 	TFile * outFile = new TFile(dir+Form("%s_reweighted_%s.root", targetName.Data(), regionName.Data()), "recreate");
 	TH1F * outHist_1 = (TH1F*)targetHist_1->Clone(Form("sumHisto_%s_%s", varNames[0].Data(), regionName.Data()));
@@ -76,6 +83,8 @@ int main(int argc, char** argv)
 	TH1F * outHist_3 = (TH1F*)targetHist_3->Clone(Form("sumHisto_%s_%s", varNames[2].Data(), regionName.Data()));
 	TH1F * outHist_4 = (TH1F*)targetHist_4->Clone(Form("sumHisto_%s_%s", varNames[3].Data(), regionName.Data()));
 	TH1F * outHist_5 = (TH1F*)targetHist_5->Clone(Form("sumHisto_%s_%s", varNames[4].Data(), regionName.Data()));
+	TH1F * outHist_6 = (TH1F*)targetHist_5->Clone(Form("sumHisto_%s_%s", varNames[5].Data(), regionName.Data()));
+	TH1F * outHist_7 = (TH1F*)targetHist_5->Clone(Form("sumHisto_%s_%s", varNames[6].Data(), regionName.Data()));
 
 	float purity, N_data, N_ttyy, N_target;	
 	N_data = dataHist_1->Integral();
@@ -148,6 +157,9 @@ int main(int argc, char** argv)
 	TH1F * yyHist_3 = (TH1F *) yyFile->Get(Form("sumHisto_%s_%s", varNames[2].Data(), regionName.Data()));
 	TH1F * yyHist_4 = (TH1F *) yyFile->Get(Form("sumHisto_%s_%s", varNames[3].Data(), regionName.Data()));
 	TH1F * yyHist_5 = (TH1F *) yyFile->Get(Form("sumHisto_%s_%s", varNames[4].Data(), regionName.Data()));
+	TH1F * yyHist_6 = (TH1F *) yyFile->Get(Form("sumHisto_%s_%s", varNames[5].Data(), regionName.Data()));
+	TH1F * yyHist_7 = (TH1F *) yyFile->Get(Form("sumHisto_%s_%s", varNames[6].Data(), regionName.Data()));
+
 	float new_N_target = yyHist_1->Integral();
 	if (targetName.Contains("yybj") || targetName.Contains("yycj") || targetName.Contains("yylj")) {
 		N_target = new_N_target;
@@ -164,6 +176,9 @@ int main(int argc, char** argv)
 	outHist_3->Scale(SF);
 	outHist_4->Scale(SF);
 	outHist_5->Scale(SF);
+	outHist_6->Scale(SF);
+	outHist_7->Scale(SF);
+
 	cout<<N_target<<" -> "<<outHist_1->Integral()<<endl;
 
 	outFile->cd();
@@ -172,5 +187,7 @@ int main(int argc, char** argv)
 	outHist_3->Write();
 	outHist_4->Write();
 	outHist_5->Write();
+	outHist_6->Write();
+	outHist_7->Write();
 	outFile->Close();
 }
